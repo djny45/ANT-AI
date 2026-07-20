7c909b133054090786cb6d6882a2ea0b28e5e680
djny45 <40324429+djny45@users.noreply.github.com>
2026-07-20 08:40:04 +0530

    Add ANT Android bridge layer

---
+++ android/android_bridge.py
@@ -0,0 +1,8 @@
+"""ANT AI Android integration bridge."""
+
+class AndroidBridge:
+    def connect(self, device):
+        return {
+            "device": device,
+            "status": "connected"
+        }
