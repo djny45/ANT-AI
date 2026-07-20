2222c96012153eab545803665a7ea9dfeba979fb
djny45 <40324429+djny45@users.noreply.github.com>
2026-07-20 10:50:27 +0530

    Add ANT documentation reader

---
+++ knowledge_engine/documentation_reader.py
@@ -0,0 +1,8 @@
+"""ANT AI documentation understanding layer."""
+
+class DocumentationReader:
+    def read(self, document):
+        return {
+            "document": document,
+            "status": "processed"
+        }
