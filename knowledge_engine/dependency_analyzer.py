c6c61f8d08423693e37fa3696a1eba4ff48eb709
djny45 <40324429+djny45@users.noreply.github.com>
2026-07-20 10:50:20 +0530

    Add ANT dependency analyzer

---
+++ knowledge_engine/dependency_analyzer.py
@@ -0,0 +1,8 @@
+"""ANT AI dependency analysis."""
+
+class DependencyAnalyzer:
+    def analyze(self, project):
+        return {
+            "project": project,
+            "dependencies": []
+        }
