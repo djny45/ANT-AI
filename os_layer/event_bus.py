a241d17d301c54ead5772890e7bef1f13f481168
djny45 <40324429+djny45@users.noreply.github.com>
2026-07-20 08:22:46 +0530

    Add ANT event communication bus

---
+++ os_layer/event_bus.py
@@ -0,0 +1,11 @@
+"""ANT AI internal event bus."""
+
+class EventBus:
+    def __init__(self):
+        self.events = []
+
+    def publish(self, event):
+        self.events.append(event)
+
+    def get_events(self):
+        return self.events
