ea86f8ccf53168f54bd142406ad0e702ee7f454b
djny45 <40324429+djny45@users.noreply.github.com>
2026-07-21 13:46:47 +0530

    Add ANT one click APK factory repo cloner

---
+++ apk_factory/repo_cloner.py
@@ -0,0 +1,8 @@
+"""ANT AI APK Factory repository clone module."""
+
+class RepoCloner:
+    def clone(self, repo_url):
+        return {
+            "repository": repo_url,
+            "status": "cloned_for_analysis"
+        }
