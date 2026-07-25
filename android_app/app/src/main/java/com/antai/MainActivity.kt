e5b841ca85556a894eb2a6bd2b2480acb2048101
djny45 <40324429+djny45@users.noreply.github.com>
2026-07-26 00:22:24 +0530

    Connect MainActivity to ANT AI themed interface

--- android_app/app/src/main/java/com/antai/MainActivity.kt
+++ android_app/app/src/main/java/com/antai/MainActivity.kt
@@ -3,13 +3,16 @@ package com.antai
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.antai.ui.ANTApp
+import com.antai.ui.screens.HomeScreen
+import com.antai.ui.theme.AntTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ANTApp()
+            AntTheme(darkTheme = true) {
+                HomeScreen()
+            }
         }
     }
 }
