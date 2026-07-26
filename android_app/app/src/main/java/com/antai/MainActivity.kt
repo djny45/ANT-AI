fc17472b7e0a2ec61c710259977ea25642b51355
djny45 <40324429+djny45@users.noreply.github.com>
2026-07-26 16:28:21 +0530

    Improve ANT CLAW Compose IME handling

--- android_app/app/src/main/java/com/antai/MainActivity.kt
+++ android_app/app/src/main/java/com/antai/MainActivity.kt
@@ -3,15 +3,25 @@ package com.antai
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.ui.Modifier
 import com.antai.ui.screens.HomeScreen
 import com.antai.ui.theme.AntTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             AntTheme(darkTheme = true) {
-                HomeScreen()
+                androidx.compose.foundation.layout.Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .imePadding()
+                ) {
+                    HomeScreen()
+                }
             }
         }
     }
