88e113ef06ff88c0519ed7e4f6036674d33fb0e1
djny45 <40324429+djny45@users.noreply.github.com>
2026-08-21 05:33:30 +0530

    chore: remove obsolete APK factory

--- apk_factory/repo_cloner.py
+++
@@ -1,8 +0,0 @@
-"""ANT AI APK Factory repository clone module."""
-
-class RepoCloner:
-    def clone(self, repo_url):
-        return {
-            "repository": repo_url,
-            "status": "cloned_for_analysis"
-        }
