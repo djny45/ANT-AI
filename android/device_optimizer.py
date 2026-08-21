92ba49a809e5201169441c88e87f676b2f37e3e8
djny45 <40324429+djny45@users.noreply.github.com>
2026-08-21 05:32:15 +0530

    chore: remove obsolete Android components

--- android/device_optimizer.py
+++
@@ -1,8 +0,0 @@
-"""ANT AI Android performance optimizer."""
-
-class DeviceOptimizer:
-    def analyze(self, device):
-        return {
-            "device": device,
-            "recommendations": ["optimize_memory", "reduce_background_tasks"]
-        }
