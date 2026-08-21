0fc8767c34ef762d26b8ea05fb1127e186e8f9de
djny45 <40324429+djny45@users.noreply.github.com>
2026-08-21 05:32:19 +0530

    chore: remove obsolete Android components

--- android/notification_agent.py
+++
@@ -1,8 +0,0 @@
-"""ANT AI Android notification manager."""
-
-class NotificationAgent:
-    def send(self, message):
-        return {
-            "notification": message,
-            "status": "sent"
-        }
