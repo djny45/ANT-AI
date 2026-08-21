6c89a94c78d7cd4b73b63839781ab002675ffb29
djny45 <40324429+djny45@users.noreply.github.com>
2026-08-21 05:32:11 +0530

    chore: remove obsolete Android components

--- android/android_bridge.py
+++
@@ -1,8 +0,0 @@
-"""ANT AI Android integration bridge."""
-
-class AndroidBridge:
-    def connect(self, device):
-        return {
-            "device": device,
-            "status": "connected"
-        }
