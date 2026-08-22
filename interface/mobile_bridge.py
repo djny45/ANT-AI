e868e98ab3fe69ee6afbc462d53adb5d78f14036
djny45 <40324429+djny45@users.noreply.github.com>
2026-08-22 14:11:22 +0530

    chore: remove obsolete mobile bridge

--- interface/mobile_bridge.py
+++
@@ -1,8 +0,0 @@
-"""ANT AI mobile connection bridge."""
-
-class MobileBridge:
-    def connect(self, device):
-        return {
-            "device": device,
-            "status": "connected"
-        }
